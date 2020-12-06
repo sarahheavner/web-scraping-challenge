2b54deddbdbaad8105662c8c49d0faa401e8e43e
Sarah Heavner <sheavner@gmail.com>
2020-12-06 13:18:01 -0700

    updated scrape_mars and app files

--- Missions_to_Mars/app.py
+++ Missions_to_Mars/app.py
@@ -1,4 +1,42 @@
 #import dependencies
 from flask import Flask, render_template, redirect
 from flask_pymongo import PyMongo
-import scrape_mars.py
+import scrape_mars
+
+
+#initialize Flask
+app = Flask(__name__)
+
+
+# Use flask_pymongo to set up mongo connection
+mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_app")
+
+
+# Route to render index.html template using data from Mongo
+@app.route('/')
+def index():
+    mars_info = mongo.db.mars_info.find_all()
+    return render_template('index.html', mars_info=mars_info)
+
+
+
+# Route that will trigger the scrape function
+@app.route("/scrape")
+def scraper():
+
+    mars_info = mongo.db.mars_info
+
+    # Run the scrape function
+    mars_data = scrape_mars.scrape()
+
+    # Update the Mongo database using update and upsert=True
+    mars_info.update({}, mars_data, upsert=True)
+
+    #Redirect to home page
+    return redirect("/", code=302)
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
--- Missions_to_Mars/scrape_mars.py
+++ Missions_to_Mars/scrape_mars.py
@@ -1,10 +1,36 @@
 #import dependencies
 from splinter import Browser
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup as bs
+import pandas as pd
 from webdriver_manager.chrome import ChromeDriverManager
 
 
+#initialize browser
 def init_browser():
-    # @NOTE: Replace the path with your actual path to the chromedriver
     executable_path = {"executable_path": ChromeDriverManager().install()}
     return Browser("chrome", **executable_path, headless=False)
+
+
+def scrape():
+    browser = init_browser()
+    mars_info = {}
+
+    url = 'https://mars.nasa.gov/news/'
+    browser.visit(url)
+
+    html = browser.html
+    soup = bs(html, "html.parser")
+
+    titles = soup.find_all(name='div', class_='content_title')
+    title_list = [t.text for t in titles]
+    news_title = title_list[1]
+
+    paragraphs = soup.find_all(name='div', class_='article_teaser_body')
+    paragraph_list = [p.text for p in paragraphs]
+    news_p = paragraph_list[0]
+
+
+    mars_info['news_title'] = news_title
+    mars_info['news_p'] = news_p
+
+    return mars_info
